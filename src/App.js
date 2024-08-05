04a6e63119dd6b3fa493594a196f426cbbc48cd7
huyhai1994 <nguyenhuyhai926@gmail.com>
2024-08-05 12:55:52 +0700

    update Route to Components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import {Route, Routes} from "react-router-dom";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import Home from "./pages/home/Home";
+import Master from "./pages/admin/Master";
 
 function App() {
-    return (<div>
-
-    </div>);
+    return (<Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/register" element={<Register/>}/>
+        <Route path="/admin" element={<Master/>}/>
+    </Routes>);
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'react-toastify'
+
 import App from './App';
+import {BrowserRouter} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <React.StrictMode>
+root.render(<React.StrictMode>
+    <BrowserRouter>
         <App/>
-    </React.StrictMode>
-);
+    </BrowserRouter>
+</React.StrictMode>);
 
