bde2540089f19da61e09fcf995ba0e7e600f0453
huyhai1994 <nguyenhuyhai926@gmail.com>
2024-08-05 13:02:37 +0700

    update basic login form

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'font-awesome/css/font-awesome.min.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'react-toastify'
 
