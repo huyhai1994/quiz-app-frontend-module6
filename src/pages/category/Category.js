cc97661b4a7b6d461bd94e5d5b44f37ad0956320
hainam1411 <hainam141199@gmail.com>
2024-08-06 08:28:35 +0700

    fix menu category

--- src/pages/category/Category.js
+++ src/pages/category/Category.js
@@ -2,6 +2,8 @@ import "./category.css"
 import React from 'react'
 import Navbar from "../../components/navbar/Navbar";
 import Sidebar from "../../components/sidebar/Sidebar";
+import {Button} from "@mui/material";
+import {Link} from "react-router-dom";
 
 
 const rows = [
@@ -15,27 +17,65 @@ const Category = () => {
             <div className="single">
                 <Sidebar/>
                 <div className="singleContainer">
-                    <Navbar/>
-                    <div className="table" style={{marginTop:"150px"}}>
-                        <table>
-                            <thead>
-                            <tr>
-                                <th>ID</th>
-                                <th>Title</th>
-                                <th>Description</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            {rows.map((row) => (
-                                <tr key={row.id}>
-                                    <td>{row.id}</td>
-                                    <td>{row.title}</td>
-                                    <td>{row.description}</td>
-                                </tr>
-                            ))}
-                            </tbody>
-                        </table>
+                    <Navbar/><h1>Category</h1>
+                    <div className="row mb-4">
+                        <form className="d-flex col-12" role="search">
+                            <div className="col-2">
+                                <span>Danh sách từ</span>
+                                <input
+                                    className="form-control me-2"
+                                    type="date"
+                                    placeholder="Search"
+                                    aria-label="Search"
+                                />
+                            </div>
+                            <div className="col-2">
+                                <span>đến</span>
+                                <input
+                                    className="form-control me-2"
+                                    type="date"
+                                    placeholder="Search"
+                                    aria-label="Search"
+                                />
+                            </div>
+                            <button className="btn btn-outline-danger" type="submit">
+                                Xem
+                            </button>
+                        </form>
                     </div>
+                    <table className="table table-hover table-bordered" style={{textAlign: "center"}}>
+                        <thead>
+                        <tr>
+                            <th>STT</th>
+                            <th>Ten danh muc</th>
+                            <th>Mo ta</th>
+                            <th>hanh dong</th>
+
+                        </tr>
+                        </thead>
+                        <tbody>
+
+
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td>
+                                <Button>Sua</Button>
+                                <Button>Xoa</Button>
+                            </td>
+                        </tr>
+
+                        </tbody>
+                        <tr>
+                            <td colSpan={8}></td>
+                            <td>
+                                <Link to={"/"}>
+                                    <button className="btn btn-outline-info mb-2">Thêm đơn hàng</button>
+                                </Link>
+                            </td>
+                        </tr>
+                    </table>
                 </div>
             </div>
 
