e4be062ddc2812a8d7b25cf4849012c239de58e7
huyhai1994 <nguyenhuyhai926@gmail.com>
2024-08-06 11:09:35 +0700

    dựng lại cấu trúc dự án

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,81 +1,8 @@
-import "./sidebar.css";
-import DashboardIcon from "@mui/icons-material/Dashboard";
-import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
-import CreditCardIcon from "@mui/icons-material/CreditCard";
-import StoreIcon from "@mui/icons-material/Store";
-import ExitToAppIcon from "@mui/icons-material/ExitToApp";
-import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
-import {Link} from "react-router-dom";
-import {DarkModeContext} from "../../context/darkModeContext";
-import {useContext} from "react";
+import React from 'react'
 
 const Sidebar = () => {
-    const {dispatch} = useContext(DarkModeContext);
-    return (<div className="sidebar">
-        <div className="top">
-            <Link to="/" style={{textDecoration: "none"}}>
-                <span className="logo">SmartQuiz</span>
-            </Link>
-        </div>
-        <hr/>
-        <div className="center">
-            <ul>
-                <p className="title">MAIN</p>
-                <li>
-                    <DashboardIcon className="icon"/>
-                    <span>Dashboard</span>
-                </li>
-                <p className="title">LISTS</p>
-
-                <Link to="/products" style={{textDecoration: "none"}}>
-                    <li>
-                        <StoreIcon className="icon"/>
-                        <span>Quiz list</span>
-                    </li>
-                </Link>
-                <Link to="/admin/category" style={{textDecoration: "none"}}>
-                    <li>
-                        <CreditCardIcon className="icon"/>
-                        <span>Category</span>
-                    </li>
-                </Link>
-
-                <Link to="/admin/pending-list" style={{textDecoration: "none"}}>
-                    <li>
-                        <PersonOutlineIcon className="icon"/>
-                        <span>Danh sách chờ</span>
-                    </li>
-                </Link>
-
-                <Link to="/admin/teacher-list" style={{textDecoration: "none"}}>
-                    <li>
-                        <PersonOutlineIcon className="icon"/>
-                        <span>Danh sách Giáo Viên</span>
-                    </li>
-                </Link>
-                <p className="title"> USER </p>
-                <li>
-                    <AccountCircleOutlinedIcon className="icon"/>
-                    <span>Profile</span>
-                </li>
-                <li>
-                    <ExitToAppIcon className="icon"/>
-                    <span>Logout</span>
-                </li>
-            </ul>
-        </div>
-        <div className="bottom">
-            <div
-                className="colorOption"
-                onClick={() => dispatch({type: "LIGHT"})}
-            ></div>
-            <div
-                className="colorOption"
-                onClick={() => dispatch({type: "DARK"})}
-            ></div>
-        </div>
-    </div>)
-        ;
-};
-
-export default Sidebar;
+    return (
+        <div>Sidebar</div>
+    )
+}
+export default Sidebar
--- src/pages/category/Category.js
+++ src/pages/category/Category.js
@@ -1,86 +1,8 @@
-import "./category.css"
 import React from 'react'
-import Navbar from "../../components/navbar/Navbar";
-import Sidebar from "../../components/sidebar/Sidebar";
-import {Button} from "@mui/material";
-import {Link} from "react-router-dom";
 
-
-const rows = [
-    {id: 1, title: 'Category 1', description: 'Description for Category 1'},
-    {id: 2, title: 'Category 2', description: 'Description for Category 2'},
-    {id: 3, title: 'Category 3', description: 'Description for Category 3'},
-];
 const Category = () => {
     return (
-        <>
-            <div className="single">
-                <Sidebar/>
-                <div className="singleContainer">
-                    <Navbar/><h1>Category</h1>
-                    <div className="row mb-4">
-                        <form className="d-flex col-12" role="search">
-                            <div className="col-2">
-                                <span>Danh sách từ</span>
-                                <input
-                                    className="form-control me-2"
-                                    type="date"
-                                    placeholder="Search"
-                                    aria-label="Search"
-                                />
-                            </div>
-                            <div className="col-2">
-                                <span>đến</span>
-                                <input
-                                    className="form-control me-2"
-                                    type="date"
-                                    placeholder="Search"
-                                    aria-label="Search"
-                                />
-                            </div>
-                            <button className="btn btn-outline-danger" type="submit">
-                                Xem
-                            </button>
-                        </form>
-                    </div>
-                    <table className="table table-hover table-bordered" style={{textAlign: "center"}}>
-                        <thead>
-                        <tr>
-                            <th>STT</th>
-                            <th>Ten danh muc</th>
-                            <th>Mo ta</th>
-                            <th>hanh dong</th>
-
-                        </tr>
-                        </thead>
-                        <tbody>
-
-
-                        <tr>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td>
-                                <Button>Sua</Button>
-                                <Button>Xoa</Button>
-                            </td>
-                        </tr>
-
-                        </tbody>
-                        <tr>
-                            <td colSpan={8}></td>
-                            <td>
-                                <Link to={"/"}>
-                                    <button className="btn btn-outline-info mb-2">Thêm đơn hàng</button>
-                                </Link>
-                            </td>
-                        </tr>
-                    </table>
-                </div>
-            </div>
-
-        </>
-
-    );
+        <div>Category</div>
+    )
 }
-export default Category;
+export default Category
