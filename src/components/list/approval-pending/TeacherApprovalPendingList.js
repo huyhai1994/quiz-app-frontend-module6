ed290a009cf8e4750149f32c41ac26fdbf5fdf0e
huyhai1994 <nguyenhuyhai926@gmail.com>
2024-08-06 14:10:08 +0700

    render Danh sách chờ phê duyệt

--- src/components/list/approval-pending/TeacherApprovalPendingList.js
+++ src/components/list/approval-pending/TeacherApprovalPendingList.js
@@ -1,8 +1,48 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react';
+import TeacherService from '../../../services/teacher.service';
 
 const TeacherApprovalPendingList = () => {
-    return (
-        <div>TeacherApproval</div>
-    )
-}
-export default TeacherApprovalPendingList
+    const [teachers, setTeachers] = useState([]);
+
+    useEffect(() => {
+        const fetchPendingTeachers = async () => {
+            try {
+                const response = await TeacherService.getPendingTeachers();
+                setTeachers(response.data);
+            } catch (error) {
+                console.error('Error fetching pending Teacher List:', error);
+            }
+        };
+        fetchPendingTeachers();
+    }, []);
+
+    return (<div>
+        <h1>Danh sách chờ duyệt</h1>
+        <table className="table table-striped">
+            <thead>
+            <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Registered At</th>
+                <th>Last Login</th>
+                <th></th>
+            </tr>
+            </thead>
+            <tbody>
+            {teachers.map(teacher => (<tr key={teacher.id}>
+                <td>{teacher.name}</td>
+                <td>{teacher.email}</td>
+                <td>{teacher.registeredAt}</td>
+                <td>{teacher.lastLogin}</td>
+                <td>
+                    <button className="btn btn-primary">
+                        Approval
+                    </button>
+                </td>
+            </tr>))}
+            </tbody>
+        </table>
+    </div>);
+};
+
+export default TeacherApprovalPendingList;
--- src/services/teacher.service.js
+++ src/services/teacher.service.js
@@ -6,6 +6,10 @@ class TeacherService {
     static async getAllTeachers() {
         return await axios.get(API_USER_URL + '/teachers');
     }
+
+    static async getPendingTeachers() {
+        return await axios.get(API_USER_URL + '/approval-pending');
+    }
 }
 
 export default TeacherService;
