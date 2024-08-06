5342105458b505385df3609fa5cdd42bbccadc41
huyhai1994 <nguyenhuyhai926@gmail.com>
2024-08-06 09:43:38 +0700

    update lấy dữ liệu bằng axios ok

--- src/components/list/teachers/TeacherList.js
+++ src/components/list/teachers/TeacherList.js
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react';
+import TeacherService from '../../../services/teacher.service';
 
 const TeacherList = () => {
-    return (
-        <div>TeacherList</div>
-    )
-}
-export default TeacherList
+    const [teachers, setTeachers] = useState([]);
+
+    useEffect(() => {
+        const fetchTeachers = async () => {
+            try {
+                const response = await TeacherService.getAllTeachers();
+                setTeachers(response.data);
+            } catch (error) {
+                console.error('Error fetching Teacher List:', error);
+            }
+        };
+        fetchTeachers();
+    }, []);
+
+    return (<div>
+        <h1>Teacher List</h1>
+        <ul>
+            {teachers.map(teacher => (<li key={teacher.id}>{teacher.name}</li>))}
+        </ul>
+    </div>);
+};
+
+export default TeacherList;
