4df8af59601f14623260d0bbc3d02aa3b1a5a7ac
hainam1411 <hainam141199@gmail.com>
2024-08-07 08:51:43 +0700

    change category service

--- src/services/category.service.js
+++ src/services/category.service.js
@@ -1,10 +1,14 @@
 import axios from "axios";
-import {API_CATEGORIES_URL} from "../configs/backend.configs";
+import {API_ADD_CATEGORY_URL, API_CATEGORIES_URL} from "../configs/backend.configs";
 
 class CategoryService {
     static async getAllCategories() {
         return await axios.get(API_CATEGORIES_URL)
     }
+    static async addCategory(data) {
+        return await axios.post(API_ADD_CATEGORY_URL, data);
+    }
+
 }
 
 export default CategoryService;
