d235e06a3c438376a2c9dee6514ca75897c14402
huyhai1994 <nguyenhuyhai926@gmail.com>
2024-08-13 09:38:45 +0700

    update lại phần form tạo bài thi thêm phần lựa chọn câu hỏi

--- src/components/list/quiz/quiz-create/QuizzCreate.js
+++ src/components/list/quiz/quiz-create/QuizzCreate.js
@@ -2,19 +2,35 @@ import React, {useEffect, useState} from 'react';
 import {useFormik} from 'formik';
 import * as Yup from 'yup';
 import axios from 'axios';
-import {Box, Button, Grid, List, ListItem, ListItemText, TextField, Typography} from '@mui/material';
+import {
+    Box,
+    Button,
+    FormControl,
+    Grid,
+    InputLabel,
+    List,
+    ListItem,
+    ListItemText,
+    MenuItem,
+    Select,
+    TextField,
+    Typography
+} from '@mui/material';
+import Swal from 'sweetalert2';
+import './QuizzCreate.css';
 import {API_CATEGORIES_URL, API_QUESTION_URL} from '../../../../configs/backend.configs';
 
 const QuizCreate = () => {
     const [categories, setCategories] = useState([]);
     const [questions, setQuestions] = useState([]);
     const [selectedQuestions, setSelectedQuestions] = useState([]);
+    const [quantity, setQuantity] = useState('');
 
     useEffect(() => {
         const fetchCategories = async () => {
             try {
                 const response = await axios.get(API_CATEGORIES_URL);
-                setCategories(response.data); // Assuming response.data is directly usable
+                setCategories(response.data);
             } catch (error) {
                 console.error('Error fetching categories:', error);
             }
@@ -23,8 +39,7 @@ const QuizCreate = () => {
         const fetchQuestions = async () => {
             try {
                 const response = await axios.get(`${API_QUESTION_URL}/list`);
-                console.log('Response:', response); // Assuming response.data is directly usable
-                setQuestions(response.data); // Assuming response.data is directly usable
+                setQuestions(response.data);
             } catch (error) {
                 console.error('Error fetching questions:', error);
             }
@@ -44,10 +59,17 @@ const QuizCreate = () => {
             quantity: Yup.number().required('Required'),
             passingScore: Yup.number().required('Required'),
         }), onSubmit: (values) => {
-            values.questionIds = selectedQuestions.map((q) => q.questionId);
+            values.questionIds = selectedQuestions.slice(0, values.quantity).map((q) => q.questionId);
+            values.timestamp = getCurrentTimestamp();
             axios.post('/api/quiz', values)
                 .then(response => {
-                    console.log('Quiz created successfully:', response.data);
+                    Swal.fire({
+                        icon: 'success',
+                        title: 'Tạo bài thi thành công',
+                        text: 'Bạn đã tạo bài thi thành công',
+                    }).then(() => {
+                        window.location.href = '/teacher/quiz';
+                    });
                 })
                 .catch(error => {
                     console.error('Error submitting quiz:', error);
@@ -55,11 +77,26 @@ const QuizCreate = () => {
         },
     });
 
+    const getCurrentTimestamp = () => {
+        const now = new Date();
+        const pad = (num, size) => ('000' + num).slice(size * -1);
+        const date = [now.getFullYear(), pad(now.getMonth() + 1, 2), pad(now.getDate(), 2)].join('-');
+        const time = [pad(now.getHours(), 2), pad(now.getMinutes(), 2), pad(now.getSeconds(), 2)].join(':');
+        const milliseconds = pad(now.getMilliseconds(), 3);
+        return `${date} ${time}.${milliseconds}`;
+    };
+
     const handleQuestionClick = (question) => {
         if (selectedQuestions.includes(question)) {
             setSelectedQuestions(selectedQuestions.filter(q => q !== question));
-        } else {
+        } else if (selectedQuestions.length < quantity) {
             setSelectedQuestions([...selectedQuestions, question]);
+        } else {
+            Swal.fire({
+                icon: 'warning',
+                title: 'Số câu hỏi vượt quá giới hạn.',
+                text: ` Bạn chỉ có thể lựa chọn ${quantity} câu hỏi `,
+            });
         }
     };
 
@@ -76,98 +113,118 @@ const QuizCreate = () => {
 
     const groupedQuestions = groupQuestionsByCategory(questions);
 
-    return (<Box sx={{maxWidth: 1200, margin: 'auto', mt: 4, padding: 3}}>
-        <Grid container spacing={4}>
-            <Grid item xs={12} md={6}>
-                <form onSubmit={formik.handleSubmit}>
-                    <TextField
-                        label="Title"
-                        fullWidth
-                        margin="normal"
-                        id="title"
-                        name="title"
-                        value={formik.values.title}
-                        onChange={formik.handleChange}
-                        error={formik.touched.title && Boolean(formik.errors.title)}
-                        helperText={formik.touched.title && formik.errors.title}
-                    />
-                    <TextField
-                        label="Description"
-                        fullWidth
-                        margin="normal"
-                        id="description"
-                        name="description"
-                        value={formik.values.description}
-                        onChange={formik.handleChange}
-                        error={formik.touched.description && Boolean(formik.errors.description)}
-                        helperText={formik.touched.description && formik.errors.description}
-                    />
-                    <TextField
-                        label="Quiz Time"
-                        fullWidth
-                        margin="normal"
-                        id="quizTime"
-                        name="quizTime"
-                        type="number"
-                        value={formik.values.quizTime}
-                        onChange={formik.handleChange}
-                        error={formik.touched.quizTime && Boolean(formik.errors.quizTime)}
-                        helperText={formik.touched.quizTime && formik.errors.quizTime}
-                    />
-                    <TextField
-                        label="Quantity"
-                        fullWidth
-                        margin="normal"
-                        id="quantity"
-                        name="quantity"
-                        type="number"
-                        value={formik.values.quantity}
-                        onChange={formik.handleChange}
-                        error={formik.touched.quantity && Boolean(formik.errors.quantity)}
-                        helperText={formik.touched.quantity && formik.errors.quantity}
-                    />
-                    <TextField
-                        label="Passing Score"
-                        fullWidth
-                        margin="normal"
-                        id="passingScore"
-                        name="passingScore"
-                        type="number"
-                        value={formik.values.passingScore}
-                        onChange={formik.handleChange}
-                        error={formik.touched.passingScore && Boolean(formik.errors.passingScore)}
-                        helperText={formik.touched.passingScore && formik.errors.passingScore}
-                    />
-                    <Button variant="contained" fullWidth type="submit" sx={{mt: 3}}>
-                        Create Quiz
-                    </Button>
-                </form>
+    const handleQuantityChange = (event) => {
+        const value = event.target.value;
+        setQuantity(value);
+        formik.handleChange(event);
+    };
+
+    return (
+        <Box sx={{maxWidth: 1200, margin: 'auto', mt: 4, padding: 3}}>
+            <Grid container spacing={4}>
+                <Grid item xs={12} md={6}>
+                    <form onSubmit={formik.handleSubmit}>
+                        <TextField
+                            label="Tiêu đề"
+                            fullWidth
+                            margin="normal"
+                            id="title"
+                            name="title"
+                            value={formik.values.title}
+                            onChange={formik.handleChange}
+                            error={formik.touched.title && Boolean(formik.errors.title)}
+                            helperText={formik.touched.title && formik.errors.title}
+                        />
+                        <TextField
+                            label="Mô tả"
+                            fullWidth
+                            margin="normal"
+                            id="description"
+                            name="description"
+                            value={formik.values.description}
+                            onChange={formik.handleChange}
+                            error={formik.touched.description && Boolean(formik.errors.description)}
+                            helperText={formik.touched.description && formik.errors.description}
+                        />
+                        <FormControl fullWidth margin="normal">
+                            <InputLabel id="quizTime-label">Thời gian làm bài</InputLabel>
+                            <Select
+                                labelId="quizTime-label"
+                                id="quizTime"
+                                name="quizTime"
+                                value={formik.values.quizTime}
+                                onChange={formik.handleChange}
+                                error={formik.touched.quizTime && Boolean(formik.errors.quizTime)}
+                            >
+                                <MenuItem value={5}>5 phút</MenuItem>
+                                <MenuItem value={10}>10 phút</MenuItem>
+                                <MenuItem value={15}>15 phút</MenuItem>
+                                <MenuItem value={20}>20 phút</MenuItem>
+                                <MenuItem value={30}>30 phút</MenuItem>
+                                <MenuItem value={60}>60 phút</MenuItem>
+                            </Select>
+                        </FormControl>
+                        <TextField
+                            label="Số lượng câu hỏi"
+                            fullWidth
+                            margin="normal"
+                            id="quantity"
+                            name="quantity"
+                            type="number"
+                            value={quantity}
+                            onChange={handleQuantityChange}
+                            error={formik.touched.quantity && Boolean(formik.errors.quantity)}
+                            helperText={formik.touched.quantity && formik.errors.quantity}
+                        />
+                        <TextField
+                            label="Điểm đạt "
+                            fullWidth
+                            margin="normal"
+                            id="passingScore"
+                            name="passingScore"
+                            type="number"
+                            value={formik.values.passingScore}
+                            onChange={formik.handleChange}
+                            error={formik.touched.passingScore && Boolean(formik.errors.passingScore)}
+                            helperText={formik.touched.passingScore && formik.errors.passingScore}
+                        />
+                        <Button variant="contained" fullWidth type="submit" sx={{mt: 3}}>
+                            Tạo mới
+                        </Button>
+                    </form>
+                </Grid>
+                {quantity && (
+                    <Grid item xs={12} md={6} className='question-select-box'>
+                        <Typography variant="h6" sx={{mb: 2}}>Xin mời lựa chọn câu hỏi: </Typography>
+                        <List sx={{maxHeight: 400, overflow: 'auto', background: '#f0f0f0', padding: '8px'}}>
+                            {Object.keys(groupedQuestions).map((category) => (
+                                <React.Fragment key={category}>
+                                    <Typography variant="subtitle1" sx={{mt: 2}}>{category}</Typography>
+                                    {groupedQuestions[category].map((question) => (
+                                        <ListItem
+                                            key={question.questionId}
+                                            button
+                                            onClick={() => handleQuestionClick(question)}
+                                            selected={selectedQuestions.includes(question)}
+                                            sx={{
+                                                margin: '8px 0',
+                                                padding: '8px',
+                                                border: '1px solid #ccc',
+                                                borderRadius: '4px',
+                                                backgroundColor: selectedQuestions.includes(question) ? '#d3d3d3' : 'white',
+                                            }}
+                                        >
+                                            <ListItemText primary={question.questionText}/>
+                                        </ListItem>
+                                    ))}
+                                </React.Fragment>
+                            ))}
+                        </List>
+                    </Grid>
+                )}
             </Grid>
-            <Grid item xs={12} md={6}>
-                <Typography variant="h6" sx={{mb: 2}}>Select Questions:</Typography>
-                <List sx={{maxHeight: 400, overflow: 'auto', background: '#f0f0f0', padding: '8px'}}>
-                    {Object.keys(groupedQuestions).map((category) => (<React.Fragment key={category}>
-                        <Typography variant="subtitle1" sx={{mt: 2}}>{category}</Typography>
-                        {groupedQuestions[category].map((question) => (<ListItem
-                            key={question.questionId}
-                            button
-                            onClick={() => handleQuestionClick(question)}
-                            selected={selectedQuestions.includes(question)}
-                            sx={{
-                                margin: '8px 0',
-                                padding: '8px',
-                                border: '1px solid #ccc',
-                                borderRadius: '4px',
-                                backgroundColor: selectedQuestions.includes(question) ? '#d3d3d3' : 'white',
-                            }}
-                        >
-                            <ListItemText primary={question.questionText}/>
-                        </ListItem>))}
-                    </React.Fragment>))}
-                </List>
-            </Grid>
-        </Grid>
-    </Box>);
+        </Box>
+    );
 };
 
 export default QuizCreate;
