5d9e0b87b879c70c20090ddb565179341729a319
huyhai1994 <nguyenhuyhai926@gmail.com>
2024-08-16 13:37:45 +0700

    snap shot (Trước khi thay đổi)

--- src/components/ListTeacher/QuestionTeacher/QuestionTeacherList.js
+++ src/components/ListTeacher/QuestionTeacher/QuestionTeacherList.js
@@ -1,14 +1,14 @@
-import { format } from "date-fns";
-import { useDispatch, useSelector } from "react-redux";
-import { useEffect, useState } from "react";
+import {format} from "date-fns";
+import {useDispatch, useSelector} from "react-redux";
+import {useEffect, useState} from "react";
 import Page from "../../pages/Page";
-import { ListTeacherQuestion } from "../../../store/questionStore/QuestionAxios";
+import {ListTeacherQuestion} from "../../../store/questionStore/QuestionAxios";
 import Swal from "sweetalert2";
-import { TailSpin } from "react-loader-spinner";
+import {TailSpin} from "react-loader-spinner";
 
 const ListTeacherQuestions = () => {
     const dispatch = useDispatch();
-    const { questions, loading, error } = useSelector((state) => state.questions);
+    const {questions, loading, error} = useSelector((state) => state.questions);
     const [currentPage, setCurrentPage] = useState(1);
     const pageSize = 5;
     const userId = localStorage.getItem('userId');
@@ -25,8 +25,8 @@ const ListTeacherQuestions = () => {
 
     if (loading) {
         return (
-            <div className="d-flex justify-content-center align-items-center" style={{ height: '100vh' }}>
-                <TailSpin color="#00BFFF" height={80} width={80} />
+            <div className="d-flex justify-content-center align-items-center" style={{height: '100vh'}}>
+                <TailSpin color="#00BFFF" height={80} width={80}/>
             </div>
         );
     }
