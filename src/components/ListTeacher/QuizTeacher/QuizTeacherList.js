3c1afbdeb3cd559f6dd197f167b2ffec684df529
nmtien11111999 <nmtien11111999@gmail.com>
2024-08-19 14:44:12 +0700

    sua

--- src/components/ListTeacher/QuizTeacher/QuizTeacherList.js
+++ src/components/ListTeacher/QuizTeacher/QuizTeacherList.js
@@ -16,8 +16,8 @@ const ListTeacherQuizzesComponent = () => {
     const userId = localStorage.getItem('userId');
 
     useEffect(() => {
-        dispatch(ListTeacherQuizzes(2));
-    }, [dispatch, 2]);
+        dispatch(ListTeacherQuizzes(userId));
+    }, [dispatch, userId]);
 
     useEffect(() => {
         if (error) {
